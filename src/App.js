55c9dbb3377ff9a872f5b8c908133ed92efe0e73
suinming <0223314338aa@gmail.com>
2022-02-12 18:56:17 +0800

    adjust signuplogin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-//import SignupLogin from './component/SignupLogin/SignupLogin';
-import Homepage from './component/Homepage/Homepage';
+import SignupLogin from './component/SignupLogin/SignupLogin';
+//import Homepage from './component/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Homepage />
+        <SignupLogin />
       </header>
     </div>
   );
